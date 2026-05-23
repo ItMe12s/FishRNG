9af75466979a41b61b03d56fdf7f9a247ab510af
imes <shinnapat06603@varee.ac.th>
2026-05-23 15:12:41 +0700

    sol2 runtime

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
 #include <Geode/Geode.hpp>
+#include <sol/sol.hpp>
 
 using namespace geode::prelude;
+
+static sol::state g_lua;
+
+$on_mod(Loaded) {
+    g_lua.open_libraries(
+        sol::lib::base,
+        sol::lib::package,
+        sol::lib::string,
+        sol::lib::math,
+        sol::lib::table,
+        sol::lib::os,
+        sol::lib::io
+    );
+
+    log::info("sol2 lua runtime ready ({})", LUA_RELEASE);
+}
